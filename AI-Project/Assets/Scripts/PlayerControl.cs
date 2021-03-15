48b240cbf1996590e01f05644c9872e44c835d5d
David Brown <davidbrown190101@gmail.com>
2021-03-15 15:48:12 +0000

    Added basic player functionality

---
+++ AI-Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [Tooltip("Prefab for X")] [SerializeField] GameObject xPrefab;
+
+    public void PlayerClick(Transform spawnLocation)
+    {
+        Instantiate(xPrefab, spawnLocation);
+    }
+}
