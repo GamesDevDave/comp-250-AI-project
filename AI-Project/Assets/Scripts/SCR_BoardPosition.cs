18c83aadb29b145bb9acc3bb207532607c9825b1
David Brown <48055482+GamesDevDave@users.noreply.github.com>
2021-04-10 10:18:19 +0100

    Refactored project

---
+++ AI-Project/Assets/Scripts/SCR_BoardPosition.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Board
+{
+    public class SCR_BoardPosition : MonoBehaviour
+    {
+        [Header("Positions.")]
+        [Tooltip("X board position.")]
+        [SerializeField] private int x;
+        [Tooltip("Y board position.")]
+        [SerializeField] private int y;
+
+        public SCR_BoardPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+    }
+}
+
